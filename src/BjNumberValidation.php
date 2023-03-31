45ced3036a8a48ff2c4f3dde3eb3393b889be55d
borg_odl <diborgiaso@gmail.com>
2023-03-31 12:42:11 +0100

    add class and verifyNumber method

---
+++ src/BjNumberValidation.php
@@ -0,0 +1,33 @@
+<?php
+namespace Odl\BjNumberValidation;
+
+final class BjNumberValidation 
+{
+    public function verifyNumber(int $number) {
+        $response = false;
+        
+        if (strlen($number) !== 8) {
+
+            $response = false;
+
+        }else{
+
+            $twoFirstNumber = substr((string)$number, 0, 2);
+
+            $prefixlIST = ['40','41','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65',
+                            '66','67','68','69','90','91','94','95','96','97','98','99'];
+
+            if(in_array($twoFirstNumber,$prefixlIST)) {
+
+                $response = true;
+
+            }else {
+
+                $response = false;
+
+            }
+        }
+
+        return $response;
+    }
+}
